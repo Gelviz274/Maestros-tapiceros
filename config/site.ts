565d21d516cfd694aac2bfb436b6d45115164d6e
Juan David Gelviz <jgelvizjaimes13@gmail.com>
2025-01-06 00:40:43 -0500

    metadatos

--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,9 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Maestros Tapiceros | Servicios de Tapicería y Restauración en Madrid | Tapizado de Muebles, Automóviles y Más",
+  name: "Maestros Tapiceros | Servicios de Tapicería y Restauración en Madrid",
   description:
-    "Descubre los servicios de Maestros Tapiceros en Madrid: tapicería de muebles, restauración de sofás y sillas, tapizado automotriz y aviación, fabricación de cabeceros y basicamas a medida, tapizado náutico y de toldos, y proyectos personalizados. Calidad, experiencia y atención al detalle para transformar y renovar tus espacios.",
+    "Descubre los servicios de Maestros Tapiceros en Madrid: tapicería de muebles, restauración de sofás y sillas, tapizado automotriz y aviación, fabricación de cabeceros y basicamas a medida, tapizado náutico y de toldos, y proyectos personalizados. Calidad y atención al detalle.",
   navItems: [
     {
       label: "Inicio",
